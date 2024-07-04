095956f8c7920e9ca56a5552b035d1b402911102
Mavi <160594705+maviasalemuhmmad@users.noreply.github.com>
2024-07-04 08:51:00 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,88 @@
+//string type 
+// console.log('mavia');
+// console.log('baloch');
+// console.log(`saab`);
+// console.log(typeof );
+// //number type 
+// console.log(123456)
+// console.log(1000000)
+// console.log(10000000)
+// // console.log( typeof 10000 )
+// console.log('2==3');
+// console.log(typeof'mavia');
+//variables declaration:(let) (const) (var)
+//box        contsiner    my variables 
+//(apple )   ( ball )       ( flowers )
+// arithmetic opretars
+// let numberofFaculty : number = 4;
+// let numberofManagemint :number = 3;
+// let numberofAbsentFaculty : 2;
+// console.log (numberofFaculty);
+// template litral 
+// let totalweightorBaryani = 70; //70 kg
+// let heightinMeters = 1.75; // 1.75m
+// let bmi = weightinkg /(heightinMeters)
+// console.log ( totalWeightofBaryani/totalnumberofFaculty); 
+// let firstName = "mavia";
+// let age =22;
+// `Backtics`
+// let message =` hello my name is : ${firstName} and my age is : $(age)  22`
+// console.log (message)
+//unary opretors
+// let index = 4;                                                                                                                         
+// ++ indeg  x
+// ++ index;//5
+// -- index;//4
+// ++ index;//5
+// -- index;//4
+// ++ index;//5
+// console.log (index)
+// what is this diffect
+// 11 + 12 + 8 + 40
+// let index =10;
+// let result = ++index + ++index + 8 + --index -2
+// console.log(result);
+// comparison operators
+// let num1 = 20 ;
+// num1 -= 22
+// console.log(num1)
+// num1 = num1 - 22
+// comparison operators
+// let num1 = 20; 
+// let num2 = 50 ;
+// console.log (num1 == num2 )
+// let num1 = 20;
+// let num2 =40;
+// console.log(num1 != num2 )
+// let num1 = 20;
+// let num1 = 20
+// let num2 =20
+// console.log (num1 > num2)
+// let age = 10 ;
+// console.log (age >= 10 )
+// let age =18 ;
+// console .log (age <= 18)
+// 1 unary opretors
+// let index = 4;
+// ++ index;//5 
+// ++index;//4
+// --index;//5
+// ++index;//4
+// --index;//5
+// console.log(index)
+// 11 + 12 + 8;
+// let index =10;
+// let result = ++index + ++index + 8 + --index -2
+// console.log(result);
+// let number = (1234567);
+// let number2 = (2353656748);
+// let number3 = (`Mavia`);
+// let number4 = (true > false)
+// console.log(number2,number3,number4);
+var num1;
+var num2;
+var num3;
+var number1 = ("mavia");
+var number2 = (123455);
+var number3 = ("Kamran");
+console.log(typeof number1, number2, number3);
